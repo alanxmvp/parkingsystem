e97d578ca7158215d2ff870c8f40d3b643f87aa5
Alan Teoh <alan_teoh@dell.com>
2019-07-12 18:02:08 +0800

    initial setup completed on spring and typeorm

---
+++ spring/parkingsystem/src/main/java/com/example/parkingsystem/entities/User.java
@@ -0,0 +1,59 @@
+package com.example.parkingsystem.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * User
+ */
+@Entity(name = "users")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "password")
+  private String password;
+
+  @Column(name = "role")
+  private String role;
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getRole() {
+    return this.role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+}
---
+++ spring/parkingsystem/src/main/java/com/example/parkingsystem/repositories/UserRepository.java
@@ -0,0 +1,23 @@
+package com.example.parkingsystem.repositories;
+
+import java.util.List;
+
+import com.example.parkingsystem.entities.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * UserRepository
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+  // List<User> findAllByEmail(String email);
+
+  // List<User> findAllByFirstName(String firstName);
+
+  // List<User> findAllByLastName(String lastName);
+
+  // List<User> findAllByPhone(String phone);
+
+  // List<User> findAllByFirstNameAndEmail(String firstName, String email);
+
+}
