a541edb8ecbfe26019548f6037a4daed8c4ba3fa
JinxCY93 <vincentjinx@hotmail.com>
2019-07-18 00:16:17 +0800

    WIP

--- angular/parkingsystem/src/app/parkinglot.service.ts
+++ angular/parkingsystem/src/app/parkinglot.service.ts
@@ -5,13 +5,44 @@ import { BehaviorSubject } from 'rxjs';
 
 const baseUrl = 'api url'
 
+const httpOptions ={
+    headers: new HttpHeaders({
+        'Content-Type': 'aplication/json',
+    }),
+}
 
-@Injectable(
-    {
-        providedIn:'root',
-    }
-)
+interface parkingLot{
+    id : number
+    location_id: number
+    status: number
+    user_id: number
+}
+
+@Injectable({
+    providedIn:'root',
+})
 
 export class parkingLotService{
+    parkingLots = new BehaviorSubject<parkingLot[]>([])
+    
+    constructor(private http: HttpClient){
+        this.http.get(`${baseUrl}/parkingLots`).subscribe((response:any[]) => {
+            let formattedParkingLot =[]
+            for (let parkingLot of response){
+                formattedParkingLot.push({
+                    id:parkingLot.id,
+                    location_id:parkingLot.location_id,
+                    status: parkingLot.status,
+                    user_id: parkingLot.user_id
+                })
+            }
+            this.parkingLots.next(formattedParkingLot)
+        })
+    }
+
+    getParkingLots(){
+        return this.parkingLots
+    }
+
     
 }
