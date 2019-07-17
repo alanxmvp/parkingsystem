7065a9005a198b3477a3c24dc139a11d5ee25181
JinxCY93 <vincentjinx@hotmail.com>
2019-07-17 22:46:45 +0800

    adding other component

---
+++ angular/parkingsystem/src/app/parkinglot.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { BehaviorSubject } from 'rxjs';
+
+
+const baseUrl = 'api url'
+
+
+@Injectable(
+    {
+        providedIn:'root',
+    }
+)
+
+export class parkingLotService{
+    
+}
